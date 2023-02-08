6327d618fa0c8a3efe979e1a61e63e96620de56a
jphustman <jphustman>
2023-02-08 14:31:36 -0900

    registration working again

--- src/Controller/Auth.php
+++ src/Controller/Auth.php
@@ -8,6 +8,8 @@ use App\CustomResponse as Response;
 use DI\Container;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+use App\Models\Users;
+
 final class Auth
 {
     private Container $container;
@@ -17,14 +19,40 @@ final class Auth
     public function __construct(Container $container)
     {
         $this->container = $container;
-        $this->db
-        $this->model = new UsersModel($db);
+        $db = $this->container->get('db');
+        $this->model = new Users($db);
     }
 
     public function register(Request $request, Response $response): Response
     {
+        $params = (array)$request->getParsedBody();
+        $db = $this->container->get('db');
 
-        return $response->withJson($message);
+        $username = $params['username'];
+        $email = $params['email'];
+        $password = $params['password'];
+
+        // Hash the password
+        $password = password_hash($password, PASSWORD_BCRYPT);
+
+        // Create a new user
+        $result = $this->model->create([
+            'username' => $username,
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        if ($result['status'] === 'Error') {
+            return $response->withJson($result);
+        }
+
+        $status = [
+            'status' => 'Success',
+            'message' => 'User successfully registered',
+            'timestamp' => time(),
+        ];
+
+        return $response->withJson($status);
     }
 
     public function signin(Request $request, Response $response): Response
--- src/Models/Users.php
+++ src/Models/Users.php
@@ -18,12 +18,69 @@ class Users {
         return $sql->fetch();
     }
 
-    public function getByLogin($login) {
+    public function getByLogin($data) {
         $sql = $this->db->prepare("select * from $this->table where password = :login");
-        $sql->bindValue(':login', $login);
+        $sql->bindValue(':login', $data['password']);
         $sql->execute();
 
         return $sql->fetch();
+    }
+
+    public function getByUsername($username) {
+        $sql = $this->db->prepare("SELECT id from $this->table where username = :username");
+        $sql->bindValue(':username', $username);
+        $sql->execute();
+
+        return $sql->fetch();
+    }
+
+    public function getByEmail($email) {
+        $sql = $this->db->prepare("SELECT id from $this->table where email = :email");
+        $sql->bindValue(':email', $email);
+        $sql->execute();
+
+        return $sql->fetch();
+    }
+    public function create(array $data): array
+    {
+        $username = $data['username'];
+        $email = $data['email'];
+        $password = $data['password'];
+
+        // Check if username already exists
+        $existingUsername = $this->getByUsername($username);
+        if ($existingUsername) {
+            return [
+                'status' => 'Error',
+                'message' => 'Username already registered!',
+            ];
+        }
+
+        // Check if email already exists
+        $existingEmail = $this->getByEmail($email);
+        if ($existingEmail) {
+            return [
+                'status' => 'Error',
+                'message' => 'Email already registered!',
+            ];
+        }
+
+        $sql = $this->db->prepare("insert into $this->table (username, email, password) values (:username, :email, :password)");
+        $sql->bindValue(':username', $data['username']);
+        $sql->bindValue(':email', $data['email']);
+        $sql->bindValue(':password', $data['password']);
+
+        if ($sql->execute()) {
+            return [
+                'status' => 'Success',
+                'message' => 'User created successfully!'
+            ];
+        } else {
+            return [
+                'status' => 'Error',
+                'message' => 'User creation failed!'
+            ];
+        };
     }
 
     public function updateLastLogin($id) {
