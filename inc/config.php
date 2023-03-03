b90ee4928053d7f53e48f2aa51d9debca52f758b
jphustman <jphustman>
2023-03-02 20:22:55 -0900

    remove old directories

--- inc/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("DB_HOST", "localhost");
-define("DB_USERNAME", "leagues-dev");
-define("DB_PASSWORD", "K-l71*5)1V<{b[*7");
-define("DB_DATABASE_NAME", "staging");
-define("SECRETKEY", "SuperSecretKeyForTradingLeagues!");
